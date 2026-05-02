015f6f227268d8cf9b6d0f8f2f1ad221ef67bd0a
smedinaf <169066790+smedinaf@users.noreply.github.com>
2026-05-02 12:24:23 -0500

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -10,7 +10,7 @@ from streamlit_drawable_canvas import st_canvas
 # ─────────────────────────────
 # CONFIG
 # ─────────────────────────────
-st.set_page_config(page_title='Tablero Inteligente 💖', layout='wide')
+st.set_page_config(page_title='Tablero para Guiones 💖', layout='wide')
 
 # 🎀 GIRLY STYLE
 st.markdown("""
